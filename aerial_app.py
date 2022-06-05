2d19cdc17a0c8f3c7264fdd98ffc7bf5e0ee0b5e
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-06-05 14:21:12 +0100

    Update aerial_app.py

--- aerial_app.py
+++ aerial_app.py
@@ -31,8 +31,8 @@ if 'count' not in st.session_state:
 if 'model' not in st.session_state:
 	#try:
 		#st.session_state.model = UNet(15)	
-		dee_model= UNet(n_channels=3, n_classes=6, bilinear=True)
-		#deep_model.cpu()
+		deep_model= UNet(n_channels=3, n_classes=6, bilinear=True)
+		deep_model.cpu()
 		deep_model.load_state_dict(torch.load('weights/unet_cpu.pt',map_location ='cpu'))
 		st.session_state.flag=True
 		st.session_state.model=deep_model
